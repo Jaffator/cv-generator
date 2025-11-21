4675bea295201e981c5b804646b5630ddb2706a2
Jaffa <jardalufi@gmail.com>
2025-11-21 11:41:21 +0100

    done

--- src/components/CVfiled.tsx
+++ src/components/CVfiled.tsx
@@ -1,4 +1,7 @@
+import React, { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
 import type { EducationData, SkillData, ExperiencenData, GeneralData } from "./CardRender";
+import { BriefcaseBusiness, HandCoins, GraduationCap } from "lucide-react";
 
 type CVPropData = {
   educatioData: EducationData[];
@@ -9,51 +12,67 @@ type CVPropData = {
 
 function CVfiled({ educatioData, SkillData, experienceData, generalData }: CVPropData) {
   return (
-    <div className="bg-gray-100 w-[60vh] h-[84vh] border flex flex-row">
+    <div className="bg-gray-100 w-[60vh] h-[84vh] border flex flex-row text-black">
       {/* side bar */}
       <div className="flex flex-col bg-lime-200 h-[calc(100%+2px)] w-1/3 items-center justify-start -m-px">
-        <img src={generalData.photo} alt="Profile" className="w-48 h-48 object-cover rounded-full p-5" />
+        {generalData.photo && (
+          <img src={generalData.photo} alt="Profile" className="w-48 h-48 object-cover rounded-full p-5" />
+        )}
         <div className="px-3 pb-3 text-left flex flex-col justify-start">
-          <h2 className="text-gray-600 text-2xl font-bold mb-2 ">
+          <h2 className="text-2xl font-bold mb-2 ">
             {generalData.firstName} {generalData.lastName}
           </h2>
-          <p className="text-gray-600 mb-4">Email: {generalData.email}</p>
-          <p className="text-gray-600 mb-4">Phone: {generalData.phone}</p>
+          <p className=" mb-4">{generalData.email && `Email: ${generalData.email}`}</p>
+          <p className="mb-4">{generalData.phone && `Phone: ${generalData.phone}`}</p>
         </div>
       </div>
       <div className="flex flex-col">
         {/* job experience */}
         <div className="flex flex-col p-3">
-          <h3 className="text-gray-800 p-2 text-lg font-bold">Job Experience</h3>
+          {experienceData.length > 0 && (
+            <div className="flex items-center flex-row gap-1">
+              <BriefcaseBusiness />
+              <h3 className="p-2 text-lg font-bold">Job Experience</h3>
+            </div>
+          )}
           {experienceData.map((exp) => (
             <div key={exp.id} className="p-2 border-b">
-              <h3 className="text-gray-800 text-lg font-semibold">
+              <h3 className="text-lg font-semibold">
                 {exp.positionTitle}- {exp.startDate} - {exp.endDate}
               </h3>
-              <p className="text-gray-800">{exp.companyName}</p>
-              <p className="text-gray-800">{exp.responsibilities}</p>
+              <p>{exp.companyName}</p>
+              <p>{exp.responsibilities}</p>
             </div>
           ))}
         </div>
         {/* education */}
         <div className="flex flex-col p-3">
-          <h3 className="text-gray-800 p-2 text-lg font-bold">Education</h3>
+          {educatioData.length > 0 && (
+            <div className="flex items-center flex-row gap-1">
+              <GraduationCap />
+              <h3 className="p-2 text-lg font-bold">Education</h3>
+            </div>
+          )}
           {educatioData.map((edu) => (
             <div key={edu.id} className="p-2 border-b">
-              <h3 className="text-gray-800 text-lg font-semibold">
-                {edu.title} - {edu.endDate}
+              <h3 className="text-lg font-semibold">
+                {edu.schoolName} - {edu.endDate}
               </h3>
-              <p className="text-gray-800">{edu.schoolName}</p>
+              <p>{edu.title}</p>
             </div>
           ))}
         </div>
         {/* skills */}
         <div className="flex flex-col p-3">
-          <h3 className="text-gray-800 p-2 text-lg font-bold">Skills</h3>
+          {SkillData.length > 0 && (
+            <div className="flex items-center flex-row gap-1">
+              <HandCoins />
+              <h3 className="p-2 text-lg font-bold">Skills</h3>
+            </div>
+          )}
           {SkillData.map((skill) => (
             <div key={skill.id} className="p-2 border-b">
-              <h3 className="text-gray-800 text-lg font-semibold">{skill.skillName}</h3>
-              <p className="text-gray-800">{skill.level}</p>
+              <h3 className="text-lg font-semibold">{skill.skillName}</h3>
             </div>
           ))}
         </div>
--- src/components/PhotoDropZone.tsx
+++ src/components/PhotoDropZone.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Button } from "./ui/button";
 
 export function PhotoDropzone({ onChange }: { onChange: (img: string) => void }) {
   const [isDragging, setIsDragging] = useState(false);
@@ -18,33 +19,51 @@ export function PhotoDropzone({ onChange }: { onChange: (img: string) => void })
   }
 
   return (
-    <div
-      className={`
+    <div className="flex flex-col gap-3">
+      <div
+        className={`
         border-2 border-dashed rounded-xl p-6 cursor-pointer
         transition-all flex flex-col items-center justify-center h-50
         ${isDragging ? "border-blue-500 bg-blue-50" : "border-gray-300"}
         ${preview ? "p-2" : ""}
       `}
-      onDragOver={(e) => {
-        e.preventDefault();
-        setIsDragging(true);
-      }}
-      onDragLeave={() => setIsDragging(false)}
-      onDrop={(e) => {
-        e.preventDefault();
-        setIsDragging(false);
-        handleFiles(e.dataTransfer.files);
-      }}
-      onClick={() => document.getElementById("photoInput")?.click()}
-    >
-      {/* Náhled */}
-      {preview ? (
-        <img src={preview} alt="Preview" className="w-40 h-40 object-cover rounded-lg shadow-md" />
-      ) : (
-        <p className="text-gray-600">Dragp and drop photo or click</p>
-      )}
+        onDragOver={(e) => {
+          e.preventDefault();
+          setIsDragging(true);
+        }}
+        onDragLeave={() => setIsDragging(false)}
+        onDrop={(e) => {
+          e.preventDefault();
+          setIsDragging(false);
+          handleFiles(e.dataTransfer.files);
+        }}
+        onClick={() => document.getElementById("photoInput")?.click()}
+      >
+        {/* Náhled */}
+        {preview ? (
+          <img src={preview} alt="Preview" className="w-40 h-40 object-cover rounded-lg shadow-md" />
+        ) : (
+          <p className="text-gray-600">Dragp and drop photo or click</p>
+        )}
 
-      <input id="photoInput" type="file" accept="image/*" className="hidden" onChange={(e) => handleFiles(e.target.files)} />
+        <input
+          id="photoInput"
+          type="file"
+          accept="image/*"
+          className="hidden"
+          onChange={(e) => handleFiles(e.target.files)}
+        />
+      </div>
+      <Button
+        variant={"secondary"}
+        className="cursor-pointer"
+        onClick={() => {
+          setPreview(null);
+          onChange("");
+        }}
+      >
+        Delete
+      </Button>
     </div>
   );
 }
